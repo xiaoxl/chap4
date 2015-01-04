a05efab73911ca1f81f651b087324e04b025bbf5
xinli_xiao <xiaoxinlixxl@gmail.com>
2015-01-03 22:20:06 -0600

    ex3.1

--- src/chap4.java
+++ src/chap4.java
@@ -1,14 +1,22 @@
+import java.util.StringTokenizer;
+
 /**
  * Created by xiao on 12/28/2014.
  */
 public class chap4 {
-    int i=1;
+    public static void main(String[] args) {
 
+        StringTokenizer date;
 
-    int hoho(){
-        System.out.println(i+i);
+        String inputDate="03/11/2014";
+        String monthDate, dayDate, yearDate;
+        date=new StringTokenizer(inputDate,"/");
+        monthDate=date.nextToken();
+        dayDate=date.nextToken();
+        yearDate=date.nextToken();
 
-        return i;
+        System.out.println(monthDate+"!!!"+yearDate+"@@@"+dayDate);
     }
+
 
 }
